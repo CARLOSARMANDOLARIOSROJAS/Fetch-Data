d83e7adec88e4df96fb45b2791464099ed3211ac
Carlos Larios <20460440@colima.tecnm.mx>
2024-05-07 14:25:00 -0600

    Ejemplo de uso de Promise All

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
-import { Fetch } from './components/Fetch';
-import { Query } from './components/Query';
-import { SWR } from './components/SWR';
-import { Axios } from 'axios';
-import { AxiosMine } from './components/AxiosMine';
+import { PromiseAll } from './components/PromiseAll';
+
 
 function App() {
   const queryClient = new QueryClient();
@@ -14,8 +11,8 @@ function App() {
 
       {/* <Fetch/> */}
       {/* <SWR/> */}
-      {/* <Query/> */}
-      <AxiosMine/>
+      {/* <Query/>  */}
+      <PromiseAll/> 
 
     </QueryClientProvider>
     // <Fetch/>
--- src/components/Query.tsx
+++ src/components/Query.tsx
@@ -1,15 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
-import React from 'react'
 
 export const Query = () => {
-
     async function fetchPhotos() {
         const response = await fetch('https://jsonplaceholder.typicode.com/photos', { mode: 'cors' });
         return response.json();
       }
-
     const {isPending, isError, data} = useQuery({queryKey: ['photos'], queryFn: fetchPhotos})
-
     if (isPending) {
         return <div>Loading...</div>;
       }
